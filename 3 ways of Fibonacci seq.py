eb3b0ecf0b98b31f9dc202a022fbeab79d2367a6
fragrant111 <45532776+fragrant111@users.noreply.github.com>
2020-02-06 20:11:41 +0800

    Update 3 ways of Fibonacci seq.py

--- 3 ways of Fibonacci seq.py
+++ 3 ways of Fibonacci seq.py
@@ -1,3 +1,8 @@
+# Student:Coco
+# Assistant:Peter
+# Scores:97
+# P.S. Done great ! You successfully finish the assignments but three points are deducted because you didn't use an expected function.
+# btw, your hard work makes me hang my head in shame.
 # Precise:
 # As we all know：fibs(0)=0,fibs(1)=1,
 #                 fibs(n)=fibs(n-1)+fibs(n-2)
@@ -10,9 +15,17 @@ for i in range(0, n):
 print(fibs[0:n])
 
 # Thinking:how to use the func.
-# def fibs(n):
-#     list =[]
-#
+'''
+def fibs(n):
+    list=[1,1]
+    if n<=2:
+        return 1
+    for i in range(n):
+        list.append(list[i]+list[i+1])
+    return list[n-1]
+print(fibs(4))
+
+'''
 
 
 """method 2 : simple multiple work"""
@@ -26,24 +39,19 @@ while i <= t:
 else:
     print(c)
 
-# # extra check(failed 5555555555555555555)
-# if a == fibs[t]:
-#     print("After inspection,the output is precisely meet your need!")
-# else:
-#     print("hmm,the answer didn't match successfully.It seems to have sth wrong with my algorithm...")
+# # extra check(failed 5555555555555555555) # Piggy just'cause the list fibs[]'s index start from 0....
+if c == fibs[t-1]:  # decrease t by 1
+    print("After inspection,the output is precisely meet your need!")
+else:
+    print("hmm,the answer didn't match successfully.It seems to have sth wrong with my algorithm...")
 
 """method 3 : recursion"""
 
-def fibs(n):
+
+def fibs(n):  # Pretty! it's quite a masterpiece for u since recursion is such a difficulty for a neophyte.
     if n == 1 or n == 2:
         return 1
     else:
         return fibs(n-1) + fibs(n-2)
 result = fibs(int(input("Q3:input the rank you want:")))
 print(result)
-
-
-
-
-
-
