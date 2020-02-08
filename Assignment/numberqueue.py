409d1af7e80b4b46f612ac82973bddc72d4ff12c
breezeiscool <wangkeyi1218@163.com>
2020-02-08 16:16:31 +0800

    Assignment 2020-2-7

---
+++ Assignment/numberqueue.py
@@ -0,0 +1,39 @@
+#得到数字拆分后的list
+
+
+def temp_list(number,size):
+    str_list = list(str(number))
+    return str_list
+
+
+number = str(19991031)
+size = len(number)
+str_list = temp_list(number, size)
+
+# 实现队列
+
+
+def queue():
+    num_queue = []
+    front = -1
+    rear = -1
+    for i in range(0, size):  # step1：queue
+        num_queue.append(str_list[i])
+        rear = rear + 1  # step2: push
+    print("".join(num_queue))
+    for i in range(0,size):
+        num_queue.append(num_queue.pop(front-i+1))
+        front = front + 1
+        rear = rear + 1
+        print("".join(num_queue))
+
+
+print(queue())
+
+
+
+
+
+
+
+
