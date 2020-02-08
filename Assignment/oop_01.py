7122ee73ce2c5d629bec47a53bfd671416b7f349
breezeiscool <wangkeyi1218@163.com>
2020-02-08 21:11:50 +0800

    Assignment 2020-2-7 02

---
+++ Assignment/oop_01.py
@@ -0,0 +1,32 @@
+class Guitar:
+
+
+    def __init__(self, size_0, color_0, brand_0):
+        self._size = size_0
+        self._color = color_0
+        self._brand = brand_0
+        self.strumming = "wing wing"
+        self.slapping = "dong dong"
+
+    def  get_brand(self):
+        return self._brand
+
+    def set_color(self, color_1):
+        self._color = color_1
+
+    def set_brand(self, brand_1):
+        self._brand = brand_1
+
+    def recital(self):
+        print(self.strumming, self.slapping)
+
+
+
+Coco_Guitar=Guitar(41, 'yellow' ,'YAMAHA')
+Coco_Guitar.set_color('white')
+Coco_Guitar.set_brand('Fender')
+Coco_Guitar.get_brand()
+print(Coco_Guitar.get_brand())
+Coco_Guitar.recital()
+
+
